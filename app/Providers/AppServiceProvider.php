8c0b20d590d7bc5fb32a5b0faac939083958f060
rakasyau <rakasyauqiferdian@gmail.com>
2025-12-02 23:23:33 +0700

    Update untuk akses lebih baik

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Paksa skema HTTPS jika aplikasi berjalan di production atau environment ngrok
+        if($this->app->environment('production') || env('APP_ENV') === 'local') {
+            URL::forceScheme('https');
+        }
     }
+
+    
 }
